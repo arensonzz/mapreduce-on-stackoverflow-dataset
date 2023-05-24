d571cd75ce93030232b567c6d8b9faba032b1120
Erkan Vatan <erkanvatan20@hotmail.com>
2023-05-25 01:31:52 +0300

    Reformat code

--- mapreduce/src/main/java/csvInputFormat/ArrayListTextWritable.java
+++ mapreduce/src/main/java/csvInputFormat/ArrayListTextWritable.java
@@ -13,12 +13,13 @@
  */
 package csvInputFormat;
 
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 import java.util.ArrayList;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.Writable;
 
 /**
  * This class is used to allow serialization and deserialization from hadoop side. Besides that, it
@@ -27,40 +28,40 @@ import org.apache.hadoop.io.Writable;
  * @author mvallebr
  */
 public class ArrayListTextWritable extends ArrayList<Text> implements Writable {
-  private static final long serialVersionUID = -6737762624115237320L;
+    private static final long serialVersionUID = -6737762624115237320L;
 
-  /*
-   * (non-Javadoc)
-   *
-   * @see org.apache.hadoop.io.Writable#write(java.io.DataOutput)
-   */
-  @Override
-  public void write(DataOutput dataoutput) throws IOException {
-    dataoutput.writeInt(this.size());
-    for (Text element : this) {
-      element.write(dataoutput);
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.apache.hadoop.io.Writable#write(java.io.DataOutput)
+     */
+    @Override
+    public void write(DataOutput dataoutput) throws IOException {
+        dataoutput.writeInt(this.size());
+        for (Text element : this) {
+            element.write(dataoutput);
+        }
     }
-  }
 
-  /*
-   * (non-Javadoc)
-   *
-   * @see org.apache.hadoop.io.Writable#readFields(java.io.DataInput)
-   */
-  @Override
-  public void readFields(DataInput datainput) throws IOException {
-    this.clear();
-    int count = datainput.readInt();
-    for (int i = 0; i < count; i++) {
-      try {
-        Text obj = Text.class.newInstance();
-        obj.readFields(datainput);
-        this.add(obj);
-      } catch (InstantiationException e) {
-        throw new IOException(e);
-      } catch (IllegalAccessException e) {
-        throw new IOException(e);
-      }
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.apache.hadoop.io.Writable#readFields(java.io.DataInput)
+     */
+    @Override
+    public void readFields(DataInput datainput) throws IOException {
+        this.clear();
+        int count = datainput.readInt();
+        for (int i = 0; i < count; i++) {
+            try {
+                Text obj = Text.class.newInstance();
+                obj.readFields(datainput);
+                this.add(obj);
+            } catch (InstantiationException e) {
+                throw new IOException(e);
+            } catch (IllegalAccessException e) {
+                throw new IOException(e);
+            }
+        }
     }
-  }
 }
