45859377d78f8e859989daa367903c98f8ac8534
Erkan Vatan <erkanvatan20@hotmail.com>
2023-05-30 16:03:37 +0300

    Add file selector

--- mapreduce/src/main/java/components/AppContainer.java
+++ mapreduce/src/main/java/components/AppContainer.java
@@ -1,7 +1,6 @@
 package components;
 
 import command.DockerCommand;
-import command.HadoopDFS;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,27 +8,35 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
 
 public class AppContainer extends JFrame {
 
-    private final JButton btnUpload = new JButton("Upload Data");
-    private final JButton btnStart = new JButton("Start");
+    private final JButton btnStart = new JButton("Start Job");
 
     private final MapReduceSelector selector = new MapReduceSelector();
-    private final JTextField txtB = new JTextField("/inputs/");
-    private final JTextField txtC = new JTextField("/outputs/");
-    private final JTextField txtD = new JTextField("../data/");
-    private final JTextField txtE = new JTextField("/inputs/");
+    private final JTextField txtB = new JTextField("/input/");
+    private final JTextField txtC = new JTextField("/output/");
+    
+    //private final JTextField txtD = new JTextField("../data/");
+    private final JFileChooser fileChooser = new JFileChooser();
+    
+    private final JLabel showSelectedFile = new JLabel();
+    private final JButton btnSelect = new JButton("Open a File...");
+    private final JButton btnUpload = new JButton("Upload Selected File");
+    private File selectedFile = null;
+    private final JTextField txtE = new JTextField("/input/");
 
     private final JLabel lblA = new JLabel("MapReduce Job:");
-    private final JLabel lblB = new JLabel("Input Data:");
-    private final JLabel lblC = new JLabel("Output Path:");
-    private final JLabel lblD = new JLabel("Source Path:");
+    private final JLabel lblB = new JLabel("HDFS Input Path:");
+    private final JLabel lblC = new JLabel("HDFS Output Path:");
+    private final JLabel lblD = new JLabel("Selected File:");
     private final JLabel lblE = new JLabel("Destination Path:");
 
     public AppContainer() {
         setTitle("Big Data Project");
-        setSize(550, 300);
+        setSize(700, 600);
         setLocation(new Point(300, 200));
         setLayout(null);
         setResizable(false);
@@ -41,36 +48,39 @@ public class AppContainer extends JFrame {
     private void initComponent() {
 
         lblA.setBounds(20, 10, 200, 30);
-        selector.setBounds(128, 10, 400, 30);
+        selector.setBounds(160, 10, 400, 30);
         add(selector);
 
         lblB.setBounds(20, 35, 200, 30);
-        txtB.setBounds(128, 35, 400, 30);
+        txtB.setBounds(160, 35, 400, 30);
 
         lblC.setBounds(20, 60, 200, 30);
-        txtC.setBounds(128, 60, 400, 30);
+        txtC.setBounds(160, 60, 400, 30);
 
-        btnStart.setBounds(128, 90, 400, 30);
+        btnStart.setBounds(160, 90, 400, 30);
         add(btnStart);
 
-        lblD.setBounds(20, 140, 200, 30);
-        txtD.setBounds(128, 140, 400, 30);
+        lblD.setBounds(20, 180, 200, 30);
+        //fileChooser.setBounds(160, 140, 400, 300);
+        btnSelect.setBounds(20, 140, 200, 30);
+        btnUpload.setBounds(160, 225, 400, 30);
+        showSelectedFile.setBounds(20, 180, 200, 30);
 
-        lblE.setBounds(20, 165, 200, 30);
-        txtE.setBounds(128, 165, 400, 30);
-
-        btnUpload.setBounds(128, 195, 400, 30);
-        add(btnUpload);
+        lblE.setBounds(20, 395, 200, 30);
+        txtE.setBounds(160, 395, 400, 30);
 
         add(lblA);
         add(lblB);
         add(lblC);
         add(lblD);
+        add(btnSelect);
+        add(btnUpload);
+        add(showSelectedFile);
         add(lblE);
 
         add(txtB);
         add(txtC);
-        add(txtD);
+        add(fileChooser);
         add(txtE);
     }
 
@@ -88,7 +98,7 @@ public class AppContainer extends JFrame {
                 btnUpload.setText("Uploading...");
                 new Thread(new Runnable() {
                     public void run() {
-                        HadoopDFS.uploadData(txtD.getText(), txtE.getText());
+                        //HadoopDFS.uploadData(txtD.getText(), txtE.getText());
                         btnUpload.setEnabled(true);
                         btnUpload.setText("Upload");
                     }
@@ -107,6 +117,25 @@ public class AppContainer extends JFrame {
                 }).start();
             }
         });
+        
+        btnSelect.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int returnVal = fileChooser.showOpenDialog(null);
+
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    selectedFile = fileChooser.getSelectedFile();
+                    //This is where a real application would open the file.
+                    try {
+                        
+                        System.out.println(file.getCanonicalPath());
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            }
+        });
+        
     }
 
     private void btnStartClick() {
